1451089336aa158c738a0d1d4ef55f9902b369c6
mareh10 <kirya.russkii@gmail.com>
2019-03-02 11:37:03 +0500

    Initial commit with class Point

---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    public class Point
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point Copy()
+        {
+            return new Point(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}; {1})", X, Y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj as Point).X == X && (obj as Point).Y == Y;
+        }
+    }
+}
