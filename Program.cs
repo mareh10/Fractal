553d60f7b5d89a41c5336d92a94cc3449851c2fe
mareh10 <kirya.russkii@gmail.com>
2019-03-02 11:54:11 +0500

    Added entry point, class Program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Segment[] s = {
+                new Segment(new Point(0, 0), new Point(1, 1)),
+                new Segment(new Point(1, 1), new Point(3, 0)),
+                new Segment(new Point(3, 0), new Point(4, 5))
+            };
+
+            var PC = new PolygonalChain(s);
+            Console.WriteLine(PC.ToString(0.5));
+        }
+    }
+}
