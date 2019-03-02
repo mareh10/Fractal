2d92adc7b46487cd97f06dd77b365b07280c1c47
mareh10 <kirya.russkii@gmail.com>
2019-03-02 11:44:57 +0500

    Added class Interval

---
+++ Interval.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    public class Interval
+    {
+        public readonly double start;
+        public readonly double end;
+
+        public Interval(double start, double end)
+        {
+            if (start < end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+            else
+            {
+                this.end = start;
+                this.start = end;
+            }
+        }
+
+        public double Center { get { return (start + end) / 2; } }
+
+        public double Length { get { return end - start; } }
+    }
+}
