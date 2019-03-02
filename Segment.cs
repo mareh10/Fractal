dd5faf59efe06a7dc3d2dcb294d79213869c3657
mareh10 <kirya.russkii@gmail.com>
2019-03-02 11:50:56 +0500

    Added class Segment, fixed solution and repository names

---
+++ Segment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractal
+{
+    public class Segment
+    {
+        public readonly Point start;
+        public readonly Point end;
+
+        public Segment(Point start, Point end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public Point[] ToPointArray(double period)
+        {
+            var lst = new List<Point>();
+            double prop = end.Y - start.Y / end.X - start.X;
+            //debug here
+            for (double i = start.X; i <= end.X; i += period)
+            {               
+                lst.Add(new Point(i, (i * prop) + start.Y));
+            }
+            return lst.ToArray();
+        }
+    }
+}
